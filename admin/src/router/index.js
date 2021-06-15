de66862527bc2ec0b9386dee7510d8b3026fa558
xielin <xielin@yealink.com>
2021-06-15 17:22:56 +0800

    【NODE-VUE】登录界面完成

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
 import CategoryList from '../views/CategoryList.vue'
@@ -24,8 +24,13 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
     path: '/',
-    name: 'Main',
+    name: 'main',
     component: Main,
     children: [
       { path: '/categories/create', component: CategoryEdit },
