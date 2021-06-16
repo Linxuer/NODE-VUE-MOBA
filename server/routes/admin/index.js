ec06e0badda9cc3b8b621f745f94521309733e57
xielin <xielin@yealink.com>
2021-06-16 16:07:43 +0800

    【NODE-VUE】服务端登录校验（jwt和assert）

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,31 +1,51 @@
 module.exports = (app) => {
   const express = require('express')
+  const jwt = require('jsonwebtoken')
+  const assert = require('http-assert')
+  const AdminUser = require('../../models/AdminUser')
   const router = express.Router({
     mergeParams: true,
   })
 
+  // 创建资源
   router.post('/', async (req, res) => {
     const model = await req.Model.create(req.body)
     res.send(model)
   })
+  // 更新资源
   router.put('/:id', async (req, res) => {
     const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
+  // 删除资源
   router.delete('/:id', async (req, res) => {
     await req.Model.findByIdAndDelete(req.params.id, req.body)
     res.send({
       success: true,
     })
   })
-  router.get('/', async (req, res) => {
-    const queryOptions = {}
-    if (req.Model.modelName === 'Category') {
-      queryOptions.populate = 'parent'
+  // 资源列表
+  router.get(
+    '/',
+    async (req, res, next) => {
+      const token = String(req.headers.authorization || '').split(' ').pop()
+      assert(token, 401, '请先登录')
+      const { id } = jwt.verify(token, app.get('secret'))
+      assert(id, 401, '请先登录')
+      req.user = await AdminUser.findById(id)
+      assert(req.user, 401, '请先登录')
+      await next()
+    },
+    async (req, res) => {
+      const queryOptions = {}
+      if (req.Model.modelName === 'Category') {
+        queryOptions.populate = 'parent'
+      }
+      const items = await req.Model.find().setOptions(queryOptions).limit(10)
+      res.send(items)
     }
-    const items = await req.Model.find().setOptions(queryOptions).limit(10)
-    res.send(items)
-  })
+  )
+  // 资源详情
   router.get('/:id', async (req, res) => {
     const model = await req.Model.findById(req.params.id)
     res.send(model)
@@ -51,23 +71,20 @@ module.exports = (app) => {
   app.post('/admin/api/login', async (req, res) => {
     const { username, password } = req.body
     // 1.根据用户名找用户
-    const AdminUser = require('../../models/AdminUser')
     const user = await AdminUser.findOne({ username }).select('+password')
-    if (!user) {
-      return res.status(422).send({
-        message: '用户不存在',
-      })
-    }
+    assert(user, 422, '用户不存在')
     // 2.校验密码
     const isValid = require('bcrypt').compareSync(password, user.password)
-    if (!isValid) {
-      return res.status(422).send({
-        message: '密码错误',
-      })
-    }
+    assert(isValid, 422, '密码错误')
     // 3.返回token
-    const jwt = require('jsonwebtoken')
     const token = jwt.sign({ id: user._id }, app.get('secret'))
     res.send({ token })
+  })
+
+  // 错误处理函数
+  app.use(async (err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+      message: err.message,
+    })
   })
 }
