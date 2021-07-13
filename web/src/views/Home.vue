b1555c54883801a1eb42cae4f0804b2258a3ccb2
xielin <xielin@yealink.com>
2021-07-13 20:22:22 +0800

    【NODE-VUE】首页顶部轮播图片（vue swiper） 目前问题：swiper的分页按钮无法显示

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    首页
+    <swiper ref="mySwiper" :options="swiperOption">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/1.png" height="200">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/2.png" height="200">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/3.png" height="200">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
@@ -9,22 +20,23 @@
 export default {
   data () {
     return {
-
+      swiperOption: {
+        pagination: {
+          el: '.pagination-home',
+        },
+      }
     }
   },
 
-  components: {},
-
-  computed: {},
-
-  watch: {},
-
-  created () {
-
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.$swiper
+    }
   },
 
   mounted () {
-
+    console.log('Current Swiper instance object', this.swiper)
+    this.swiper.slideTo(3, 1000, false)
   },
 
   methods: {
@@ -33,6 +45,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../assets/scss/variables';
 
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: .153846rem;
+    background: map-get($colors, 'white');
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, 'info');
+    }
+  }
+}
 </style>
