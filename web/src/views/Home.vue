fb98af365f29d935183579ae8617a002373d96c3
xielin <xielin@yealink.com>
2021-07-13 16:27:17 +0800

    【NODE-VUE】主页框架和顶部菜单

---
+++ web/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    首页
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  watch: {},
+
+  created () {
+
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
